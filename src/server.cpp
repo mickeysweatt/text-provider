94ae5a31c0bf52f9ff7cd6d295a02b27fc184477
Mickey Sweatt <sweattm92@gmail.com>
2015-10-31 15:56:25 -0700

    Changed server to support multiple async clients (still single threaded)

--- src/server.cpp
+++ src/server.cpp
@@ -1,7 +1,7 @@
 #include "server.h"
 
-//
-// server.cpp
+// Based on: ()
+// server.h
 // ~~~~~~~~~~
 //
 // Copyright (c) 2003-2015 Christopher M. Kohlhoff (chris at kohlhoff dot com)
@@ -9,73 +9,60 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-
-#include <ctime>
+#include <cstdlib>
 #include <iostream>
-#include <string>
-#include <boost/bind.hpp>
-#include <boost/shared_ptr.hpp>
-#include <boost/enable_shared_from_this.hpp>
+#include <memory>
+#include <utility>
 #include <boost/asio.hpp>
+
 
 using boost::asio::ip::tcp;
 
-namespace {
-	static std::string make_string(boost::asio::streambuf& streambuf)
-    {
-	    return {buffers_begin(streambuf.data()),
-	         buffers_end(streambuf.data())};
-    }
-}
+// namespace {
+// 	static std::string make_string(boost::asio::streambuf& streambuf)
+//     {
+// 	    return {buffers_begin(streambuf.data()),
+// 	         buffers_end(streambuf.data())};
+//     }
+// }
 
-tcp_connection::pointer tcp_connection::create(boost::asio::io_service& io_service)
-{
-    return pointer(new tcp_connection(io_service));
-}
+// tcp_connection::pointer tcp_connection::create(boost::asio::io_service& io_service)
+// {
+//     return pointer(new tcp_connection(io_service));
+// }
 
-int tcp_connection::process_request()
+int session::process_command(size_t length)
 {
-    std::string command = get_command();
-    if (command == "QUIT\r\n") {
+    if (strcmp(data_,"QUIT\r\n") == 0 ) {
         return 0;
     }
-    else if (command == "SHUTDOWN\r\n") {
+    if (strcmp(data_,"SHUTDOWN\r\n") == 0 ) {
         return 1;
     }
     else {
-        return process_request();
+        do_write(length);
+        return do_read();
     }
 }
 
-std::string tcp_connection::get_command() {
-    boost::asio::streambuf read_buffer;
-    auto bytes_transferred = read_until(socket_, read_buffer, '\n');
-    std::string command = make_string(read_buffer);
-    return command;
-}
+// std::string tcp_connection::get_command() {
+//     boost::asio::streambuf read_buffer;
+//     auto bytes_transferred = read_until(socket_, read_buffer, '\n');
+//     std::string command = make_string(read_buffer);
+//     return command;
+// }
 
 
-void tcp_server::start_accept()
+void tcp_server::do_accept()
 {
-    tcp_connection::pointer new_connection = tcp_connection::create(
-        acceptor_.get_io_service());
-    std::cout << "Waiting for connection" << std::endl;
+    acceptor_.async_accept(socket_,
+        [this](boost::system::error_code ec)
+        {
+          if (!ec)
+          {
+            std::make_shared<session>(std::move(socket_))->start();
+          }
 
-    acceptor_.async_accept(new_connection->socket(),
-                           boost::bind(&tcp_server::handle_accept, this, new_connection,
-                                       boost::asio::placeholders::error));
-}
-
-void tcp_server::handle_accept(tcp_connection::pointer new_connection,
-                   const boost::system::error_code& error)
-{
-    std::cout << "Accepting connection" << std::endl;
-    int status = -1;
-    if (!error)
-    {
-        status = new_connection->process_request();
-    }
-    if (0 == status) {
-        start_accept();
-    }
+          do_accept();
+        });
 }
--- src/server.h
+++ src/server.h
@@ -1,8 +1,8 @@
 //
-// server.cpp
-// ~~~~~~~~~~
+// async_tcp_echo_server.cpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~
 //
-// Copyright (c) 2003-2015 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+// Copyright (c) 2003-2013 Christopher M. Kohlhoff (chris at kohlhoff dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -11,61 +11,87 @@
 #ifndef INCLUDED_SERVER_H
 #define INCLUDED_SERVER_H
 
-#include <boost/bind.hpp>
-#include <boost/shared_ptr.hpp>
-#include <boost/enable_shared_from_this.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <utility>
 #include <boost/asio.hpp>
 
 using boost::asio::ip::tcp;
 
-class tcp_connection
-: public boost::enable_shared_from_this<tcp_connection>
+class session
+  : public std::enable_shared_from_this<session>
 {
 public:
-    typedef boost::shared_ptr<tcp_connection> pointer;
+  session(tcp::socket socket)
+    : socket_(std::move(socket))
+  {
+  }
 
-    static pointer create(boost::asio::io_service& io_service);
-
-    tcp::socket& socket()
-    {
-        return socket_;
-    }
-
-    int process_request();
-
-    std::string get_command();
+  int start()
+  {
+    return do_read();
+  }
 
 private:
-    tcp_connection(boost::asio::io_service& io_service)
-    : socket_(io_service)
-    {
-    }
+  int do_read()
+  {
+    auto self(shared_from_this());
+    socket_.async_read_some(boost::asio::buffer(data_, max_length),
+        [this, self](boost::system::error_code ec, std::size_t length)
+        {
+          if (!ec)
+          {
+            int status = process_command(length);
+            if (status == 0) {
+                return 0;
+            }
+            else if (status != 0) {
+                socket_.get_io_service().stop();
+            }
+            else {
+                return do_read();
+            }
+          }
+          return -1;
+        });
+    return 0;
+  }
 
-    void handle_write(const boost::system::error_code& /*error*/,
-                      size_t /*bytes_transferred*/)
-    {
-    }
+  int process_command(size_t length);
 
-    tcp::socket socket_;
-    std::string message_;
+  void do_write(std::size_t length)
+  {
+    auto self(shared_from_this());
+    boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
+        [this, self](boost::system::error_code ec, std::size_t /*length*/)
+        {
+          if (!ec)
+          {
+            do_read();
+          }
+        });
+  }
+
+  tcp::socket socket_;
+  enum { max_length = 1024 };
+  char data_[max_length];
 };
 
 class tcp_server
 {
 public:
-    tcp_server(boost::asio::io_service& io_service)
-    : acceptor_(io_service, tcp::endpoint(tcp::v4(), 10322))
-    {
-        start_accept();
-    }
+  tcp_server(boost::asio::io_service& io_service)
+    : acceptor_(io_service, tcp::endpoint(tcp::v4(), 10322)),
+      socket_(io_service)
+  {
+    do_accept();
+  }
 
 private:
-    void start_accept();
+  void do_accept();
 
-    void handle_accept(tcp_connection::pointer new_connection,
-                       const boost::system::error_code& error);
-
-    tcp::acceptor acceptor_;
+  tcp::acceptor acceptor_;
+  tcp::socket socket_;
 };
-
 #endif

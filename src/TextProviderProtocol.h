2d0917f9578fea611d0e316b49828b1982af10ea
Mickey Sweatt <sweattm92@gmail.com>
2015-10-31 21:08:03 -0700

    Created simple memory cache

--- src/TextProviderProtocol.h
+++ src/TextProviderProtocol.h
@@ -1,14 +1,23 @@
 //
 //  TextProviderProtocol.h
-//  TextProvider
 //
 //  Created by admin on 10/29/15.
 //  Copyright (c) 2015 admin. All rights reserved.
 //
 
-#ifndef __TextProvider__TextProviderProtocol__
-#define __TextProvider__TextProviderProtocol__
+#ifndef INCLUDED_TEXT_PROVIDER_PROTOCOL
+#define INCLUDED_TEXT_PROVIDER_PROTOCOL
 
-#include <stdio.h>
+#include <string>
 
-#endif /* defined(__TextProvider__TextProviderProtocol__) */
+class TextProviderProtocol {
+  public:
+  	virtual ~TextProviderProtocol() = default;
+
+	virtual std::string read_n_th_line(size_t n) = 0;
+
+	virtual size_t file_length() = 0;
+};
+
+
+#endif /* INCLUDED_TEXT_PROVIDER _PROTOCOL */
---
+++ src/simple-local-cache-text-provider.h
@@ -0,0 +1,41 @@
+//
+//  SimpleLocalCacheTextProvider.h
+//
+//  Created by admin on 10/29/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#ifndef INCLUDED_SIMPLE_LOCAL_CACHE_TEXT_PROVIDER
+#define INCLUDED_SIMPLE_LOCAL_CACHE_TEXT_PROVIDER
+
+#include "TextProviderProtocol.h"
+#include <string>
+#include <vector>
+#include <stdint.h>
+
+class SimpleLocalCacheTextProvider : public TextProviderProtocol ,
+									public std::enable_shared_from_this<SimpleLocalCacheTextProvider>
+{
+  public:
+  	SimpleLocalCacheTextProvider(const std::string& filename,
+  								 size_t start_line,
+  								 size_t max_line,
+  								 size_t max_bytes = SIZE_MAX);
+
+  	~SimpleLocalCacheTextProvider() override = default;
+
+	std::string read_n_th_line(size_t n) override;
+
+	size_t file_length();
+
+ private:
+ 	std::string filename_;
+ 	std::vector<std::string> cache_;
+ 	size_t cache_start_line_;
+ 	size_t cache_end_line_;
+ 	size_t cache_max_bytes_;
+ 	mutable size_t files_num_lines_;
+};
+
+
+#endif /* INCLUDED_SIMPLE_LOCAL_CACHE_TEXT_PROVIDER */
